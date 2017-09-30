eb6f491799c9a30e27f95b258edca9d7b00dfad1
David <David0924@hotmail.ca>
2017-09-29 20:46:30 -0400

    Unit 0-02

---
+++ address-1.py
@@ -0,0 +1,10 @@
+# Created by: David Wang
+# Created on: 12-Sep-2017
+# Created for: ICS3U
+# Daily Assignment - Unit0-02
+# This program displays the address program
+
+import ui
+
+v = ui.load_view()
+v.present('sheet')
